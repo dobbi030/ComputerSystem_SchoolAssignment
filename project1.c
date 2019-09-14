b9a714976398052eab79f374a7ffcde9f82ce60c
struct silver_line <51740252+dobbi030@users.noreply.github.com>
2019-09-14 17:32:58 +0900

    Create project1.c
    To test  how (&x + i) work

---
+++ project1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+unsigned replace_byte (unsigned x, int i, unsigned char b)
+{
+//    (&x) + i
+}
+
+int main(void)
+{
+    unsigned x = 0x12345678;
+    
+    printf(&x + 1);
+    printf(&x + 2);
+    
+    return 0;
+}
